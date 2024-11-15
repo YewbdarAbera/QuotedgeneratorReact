4b2fce7791e026de98a1978bf7981dde52c52b44
yewbdar <yewbdar.grima@gmail.com>
2024-11-15 15:53:22 -0600

    fetch qoute form api and display

--- qutoe/src/App.js
+++ qutoe/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
-  return <div className="App">react app</div>;
+  const [quote, setQuote] = useState("Click the button to get a random quote.");
+  const [author, setAuthor] = useState("");
+
+  const getNewQuote = () => {
+    fetch("https://quotes-api-self.vercel.app/quote")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.quote);
+        setAuthor(data.author);
+      });
+  };
+  return (
+    <div className="container">
+      <h1>Random Quote Generator</h1>
+      <div className="quote-container">
+        <p id="quote">
+          {quote}.-{author}
+        </p>
+        <button id="get-quote" onClick={getNewQuote}>
+          Get Quote
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
